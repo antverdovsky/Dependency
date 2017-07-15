faaece19b2c7935e05553e143ec43f20b8a6b63b
Anatoly Tverdovsky <antverdovsky@gmail.com>
2017-07-15 14:41:45 -0500

    Removed Utils File
    Moved the contents of the utils file and removed the file since it
    doesn't really make sense to have a utilities file with only one
    utility.

--- dependency_file/dependency_file.cpp
+++ dependency_file/dependency_file.cpp
@@ -159,6 +159,31 @@ std::string getFileName(CPUState *cpu, int fd, bool debug) {
 	return "";
 }
 
+std::string getGuestString(CPUState *cpu, size_t maxSize, target_ulong addr) {
+	// Create an empty string with all zeros
+	std::string str(maxSize, '0');
+	
+	for (size_t i = 0; i < maxSize; ++i) {
+		// Fetch the unsigned integer character from PANDA's memory. We do this
+		// by specifiying the CPU state, the address at which we want to read
+		// from (equal to starting address + offset), the pointer to which we
+		// want to write the value, 1 to indicate we are reading one byte at a 
+		// time, and 0 to indicate we are not writing anything to memory.
+		uint8_t uiChar = 0;
+		panda_virtual_memory_rw(cpu, addr + i, &uiChar, 1, 0);
+		
+		// Write the character to the string and check if its the null
+		// terminator character. If so, trim the string and return.
+		str.at(i) = (char)(uiChar);
+		if (str.at(i) == '\0') {
+			str = str.substr(0, i);
+			return str;
+		}
+	}
+	
+	return str;
+}
+
 void labelBufferContents(CPUState *cpu, target_ulong vAddr, uint32_t length) {
 	if (!taint2_enabled()) return;
 	if (dependency_file.debug) {
--- dependency_file/dependency_file_def.h
+++ dependency_file/dependency_file_def.h
@@ -282,6 +282,27 @@ void cbf_writeReturn(CPUState *cpu, target_ulong pc, uint32_t fd,
 std::string getFileName(CPUState *cpu, int fd, bool debug = false);
 
 /// <summary>
+/// Returns a string fetched from the specified memory address.
+/// </summary>
+/// <param name="cpu">
+/// The CPU State pointer.
+/// </param>
+/// <param name="maxSize">
+/// The maximum size of the string to be read from memory. If the actual string
+/// size exceeds this parameter, only the first maxSize number of characters
+/// will be returned. The actual string size may be smaller than this value, as
+/// this function will automatically stop when a null terminator character is
+/// encountered in memory.
+/// </param>
+/// <param name="addr">
+/// The address from which the string is to be read.
+/// </param>
+/// <returns>
+/// The guest's string at the memory location.
+/// </returns>
+std::string getGuestString(CPUState *cpu, size_t maxSize, target_ulong addr);
+
+/// <summary>
 /// Taints the contents of the buffer at the specified virtual address and of 
 /// the specified length. This function does nothing if taint2 is not currently
 /// enabled.
