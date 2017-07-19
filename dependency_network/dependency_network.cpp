9dc30901e1eeb34bbf622f6fa5b23f9ea60315bf
Anatoly Tverdovsky <antverdovsky@gmail.com>
2017-07-18 19:53:14 -0500

    Implemented SocketCall Callback Functions
    Implemented the enter and return callback functions for the socket_call
    system call.

--- dependency_network/dependency_network.cpp
+++ dependency_network/dependency_network.cpp
@@ -1,12 +1,44 @@
-#include "panda/plugin.h"
-#include "panda/plugin_plugin.h"
-
 #include "dependency_network_def.h"
 
-bool init_plugin(void *self) {
-	printf("Hello World from Dependency_Network Plugin.");
+#include <iostream>
 
+void cbf_socketCallEnter(CPUState *cpu, target_ulong pc, int32_t call,
+		uint32_t args) {
+	std::cout << "dependency_network: socket_call_enter called at " <<
+		rr_get_guest_instr_count() << ", call type: " << call << "." << 
+		std::endl;
+}
+
+void cbf_socketCallReturn(CPUState *cpu, target_ulong pc, int32_t call,
+		uint32_t args) {
+	std::cout << "dependency_network: socket_call_return called at " <<
+		rr_get_guest_instr_count() << ", call type: " << call << "." << 
+		std::endl;
+}
+
+bool init_plugin(void *self) {
+#ifdef TARGET_I386
+	dependency_network.plugin_ptr = self;
+	
+	/// Load dependent plugins
+	panda_require("osi");
+	assert(init_osi_api());
+	
+	panda_require("osi_linux");
+	assert(init_osi_linux_api());
+	
+	panda_require("syscalls2");
+	
+	// Register SysCalls2 Callback Functions
+	PPP_REG_CB("syscalls2", on_sys_socketcall_enter, cbf_socketCallEnter);
+	PPP_REG_CB("syscalls2", on_sys_socketcall_return, cbf_socketCallReturn);
+	
 	return true;
+#else
+	std::cout << "dependency_network is only supported for i386 targets." << 
+		std::endl;
+	return false;
+#endif
 }
 
 void uninit_plugin(void *self) {
--- dependency_network/dependency_network_def.h
+++ dependency_network/dependency_network_def.h
@@ -1,27 +1,82 @@
 #ifndef DEPENDENCY_NETWORK_H
 #define DEPENDENCY_NETWORK_H
 
+#include "panda/plugin.h"
+#include "panda/plugin_plugin.h"
+
+#include "taint2/taint2.h"
+
+extern "C" {
+	#include "panda/addr.h"
+	
+	#include "osi/osi_types.h"
+	#include "osi/osi_ext.h"
+	#include "osi_linux/osi_linux_ext.h"
+	#include "syscalls2/gen_syscalls_ext_typedefs.h"
+	#include "taint2/taint2_ext.h"
+}
+
 struct Dependency_Network {
 	void *plugin_ptr = nullptr;        // The plugin pointer
 };
 
 /// <summary>
-/// Initializes this plugin using the specified plugin pointer.
+/// Callback function for the "on_sys_socketcall_enter_t" system call.
 /// </summary>
-/// <param name="self">
-/// The plugin pointer passed in from PANDA.
+/// <param name="cpu">
+/// The CPU State pointer.
 /// </param>
-/// <returns>
-/// True if the plugin was successfully loaded, false otherwise.
-/// </returns>
-bool init_plugin(void *self);
-
+/// <param name="pc">
+/// The program counter.
+/// </param>
+/// <param name="call">
+/// The integer ID of the socket call.
+/// </param>
+/// <param name="args">
+/// The virtual memory address to the start of the arguments.
+/// </param>
+void cbf_socketCallEnter(CPUState *cpu, target_ulong pc, int32_t call,
+		uint32_t args);
+		
 /// <summary>
-/// Destroys this plugin.
+/// Callback function for the "on_sys_socketcall_return_t" system call.
 /// </summary>
-/// <param name="self">
-/// The plugin pointer passed in from PANDA.
+/// <param name="cpu">
+/// The CPU State pointer.
 /// </param>
-void uninit_plugin(void *self);
+/// <param name="pc">
+/// The program counter.
+/// </param>
+/// <param name="call">
+/// The integer ID of the socket call.
+/// </param>
+/// <param name="args">
+/// The virtual memory address to the start of the arguments.
+/// </param>
+void cbf_socketCallEnter(CPUState *cpu, target_ulong pc, int32_t call,
+		uint32_t args);
+
+extern "C" {
+	/// <summary>
+	/// Initializes this plugin using the specified plugin pointer.
+	/// </summary>
+	/// <param name="self">
+	/// The plugin pointer passed in from PANDA.
+	/// </param>
+	/// <returns>
+	/// True if the plugin was successfully loaded, false otherwise.
+	/// </returns>
+	bool init_plugin(void *self);
+	
+	/// <summary>
+	/// Destroys this plugin.
+	/// </summary>
+	/// <param name="self">
+	/// The plugin pointer passed in from PANDA.
+	/// </param>
+	void uninit_plugin(void *self);
+}
+
+Dependency_Network dependency_network;
 
 #endif
