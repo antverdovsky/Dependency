a3af8eb41e0093cddafe585f401d9143359d952a
antverdovsky <antverdovsky@gmail.com>
2017-08-17 11:01:30 -0500

    Move Targets to Dependency_Tracker
    Moved the target sink, target source, network target, and file target classes to the dependency_tracker file.

--- dependency_tracker/dependency_tracker.cpp
+++ dependency_tracker/dependency_tracker.cpp
@@ -8,6 +8,183 @@
 
 #include <linux/net.h>
 
+#include <typeinfo>
+
+/********************************** TARGET **********************************/
+bool Target::operator==(const Target &rhs) const {
+	return typeid(*this) == typeid(rhs);
+}
+
+bool Target::operator!=(const Target &rhs) const {
+	return typeid(*this) != typeid(rhs);
+}
+
+std::ostream& operator<<(std::ostream &stream, const Target &target) {
+	return stream << target.toString();
+}
+/********************************** TARGET **********************************/
+
+/******************************* TARGET  SINK *******************************/
+TargetSink::TargetSink(std::unique_ptr<Target> target, const size_t &index) {
+	this->target = std::move(target);
+	this->index = index;
+	
+	this->totalBytes = 0;
+	this->totalTaintBytes = 0;
+	this->totalWrites = 0;
+}
+
+const size_t& TargetSink::getIndex() const {
+	return this->index;
+}
+
+std::map<uint32_t, uint32_t>& TargetSink::getLabeledBytes() {
+	return this->labeledBytes;
+}
+
+const std::map<uint32_t, uint32_t>& TargetSink::getLabeledBytes() const {
+	return this->labeledBytes;
+}
+
+const Target& TargetSink::getTarget() const {
+	return *this->target;
+}
+
+uint32_t& TargetSink::getTotalBytes() {
+	return this->totalBytes;
+}
+
+const uint32_t& TargetSink::getTotalBytes() const {
+	return this->totalBytes;
+}
+
+uint32_t& TargetSink::getTotalTaintBytes() {
+	return this->totalTaintBytes;
+}
+
+const uint32_t& TargetSink::getTotalTaintBytes() const {
+	return this->totalTaintBytes;
+}
+
+uint32_t& TargetSink::getTotalWrites() {
+	return this->totalWrites;
+}
+
+const uint32_t& TargetSink::getTotalWrites() const {
+	return this->totalWrites;
+}
+
+/******************************* TARGET  SINK *******************************/
+
+/****************************** TARGET  SOURCE ******************************/
+TargetSource::TargetSource(std::unique_ptr<Target> target, 
+		const size_t &index) {
+	this->target = std::move(target);
+	this->index = index;
+	
+	this->labeledBytes = 0;
+	
+	this->totalBytes = 0;
+	this->totalReads = 0;
+}
+
+const size_t& TargetSource::getIndex() const {
+	return this->index;
+}
+
+uint32_t& TargetSource::getLabeledBytes() {
+	return this->labeledBytes;
+}
+
+const uint32_t& TargetSource::getLabeledBytes() const {
+	return this->labeledBytes;
+}
+
+const Target& TargetSource::getTarget() const {
+	return *this->target;
+}
+
+uint32_t& TargetSource::getTotalBytes() {
+	return this->totalBytes;
+}
+
+const uint32_t& TargetSource::getTotalBytes() const {
+	return this->totalBytes;
+}
+
+uint32_t& TargetSource::getTotalReads() {
+	return this->totalReads;
+}
+
+const uint32_t& TargetSource::getTotalReads() const {
+	return this->totalReads;
+}
+
+/****************************** TARGET  SOURCE ******************************/
+
+/******************************* TARGET  FILE *******************************/
+
+TargetFile::TargetFile() : TargetFile("") {
+
+}
+
+TargetFile::TargetFile(const std::string &name) {
+	this->fileName = name;
+}
+
+std::string TargetFile::toString() const {
+	return this->fileName;
+}
+
+TargetFile::operator bool() const {
+	return !this->fileName.empty();
+}
+
+bool TargetFile::operator==(const Target &rhs) const {
+	if (Target::operator!=(rhs)) return false;
+
+	auto rhsTF = static_cast<const TargetFile&>(rhs);
+	return this->fileName == rhsTF.fileName;
+}
+
+bool TargetFile::operator!=(const Target &rhs) const {
+	return !(this->operator==(rhs));
+}
+
+/******************************* TARGET  FILE *******************************/
+
+/****************************** TARGET NETWORK ******************************/
+
+TargetNetwork::TargetNetwork() : TargetNetwork("", 0) {
+
+}
+
+TargetNetwork::TargetNetwork(const std::string &ip, const unsigned int &port) {
+	this->ip = ip;
+	this->port = port;
+}
+
+std::string TargetNetwork::toString() const {
+	return (this->ip + "::" + std::to_string(this->port));
+}
+
+TargetNetwork::operator bool() const {
+	return !this->ip.empty();
+}
+
+bool TargetNetwork::operator==(const Target &rhs) const {
+	if (Target::operator!=(rhs)) return false;
+
+	auto rhsTN = static_cast<const TargetNetwork&>(rhs);
+	return this->ip == rhsTN.ip && this->port == rhsTN.port;
+}
+
+bool TargetNetwork::operator!=(const Target &rhs) const {
+	return !(this->operator==(rhs));
+}
+
+/****************************** TARGET NETWORK ******************************/
+
 template<typename T>
 std::vector<T> getMemoryValues(CPUState *cpu, uint32_t addr, uint32_t size) {
 	std::vector<T> values;
--- dependency_tracker/dependency_tracker_def.h
+++ dependency_tracker/dependency_tracker_def.h
@@ -22,7 +22,430 @@ extern "C" {
 	#include "taint2/taint2_ext.h"
 }
 
-#include "dependency_tracker_targets.h"
+/// <summary>
+/// Structure which represents a trackable target.
+/// </summary>
+class Target {
+public:
+	/// <summary>
+	/// Simple method which returns a string representation of this Target.
+	/// </summary>
+	/// <returns>
+	/// The string in format: "base".
+	/// </returns>
+	virtual std::string toString() const = 0;
+	
+	/// <summary>
+	/// Returns true if this Target is valid. The concept of validity is left
+	/// up to interpretation for the derived type.
+	/// </summary>
+	/// <returns>
+	/// True if the Target is valid, false otherwise.
+	/// </returns>
+	virtual explicit operator bool() const = 0;
+	
+	/// <summary>
+	/// Compares the specified Target instance for equality to this.
+	/// </summary>
+	/// <param name="rhs">
+	/// The right hand side of the operator to which this instance is to be
+	/// compared to.
+	/// </param>
+	/// <returns>
+	/// True if the instances are equivalent, false otherwise.
+	/// </returns>
+	virtual bool operator==(const Target &rhs) const;
+
+	/// <summary>
+	/// Compares the specified Target instance for inequality to this.
+	/// </summary>
+	/// <param name="rhs">
+	/// The right hand side of the operator to which this instance is to be
+	/// compared to.
+	/// </param>
+	/// <returns>
+	/// True if the instances are inequivalent, false otherwise.
+	/// </returns>
+	virtual bool operator!=(const Target &rhs) const;
+protected:
+	/// <summary>
+	/// Creates a new target.
+	/// </summary>
+	Target() = default;
+};
+
+/// <summary>
+/// Appends the toString() return value of the specified target, to the 
+/// specified output stream and returns it.
+/// </summary>
+/// <param name="stream">
+/// The stream to which the target's toString() contents are to be appended.
+/// </param>
+/// <param name="target">
+/// The target whose string value is to be appended to the stream.
+/// </param>
+/// <returns>
+/// The string stream after target has been appended to it.
+/// </returns>
+std::ostream& operator<<(std::ostream &stream, const Target &target);
+
+/// <summary>
+/// Class which represents a sink target.
+/// </summary>
+class TargetSink {
+public:
+	/// <summary>
+	/// Constructs a Target Sink with the specified target. The index should
+	/// be equivalent to the index of the Target in its sinks vector.
+	/// </summary>
+	/// <param name="target">
+	/// The unique pointer to the target.
+	/// </param>
+	/// <param name="index">
+	/// The index of this Target in the sinks vector.
+	/// </param>
+	TargetSink(std::unique_ptr<Target> target, const size_t &index);
+
+	/// <summary>
+	/// Copying of Target Source instances is forbidden.
+	/// </summary>
+	TargetSink(const TargetSink&) = delete;
+
+	/// <summary>
+	/// Returns a reference to the index of this target in the sinks vector.
+	/// This is expected to never change since targets are not added nor
+	/// removed after the plugin is initialized.
+	/// </summary>
+	const size_t& getIndex() const;
+
+	/// <summary>
+	/// Returns a reference to the map which maps the source target index to
+	/// how many tainted bytes from said source were written to this sink
+	/// target.
+	/// </summary>
+	/// <returns>
+	/// A reference to the value.
+	/// </returns>
+	std::map<uint32_t, uint32_t>& getLabeledBytes();
+	
+	/// <summary>
+	/// Returns a constant reference to the map which maps the source target 
+	/// index to how many tainted bytes from said source were written to this 
+	/// sink target.
+	/// </summary>
+	/// <returns>
+	/// A constant reference to the value.
+	/// </returns>
+	const std::map<uint32_t, uint32_t>& getLabeledBytes() const;
+
+	/// <summary>
+	/// Gets a constant reference to the target attached to this sink.
+	/// </summary>
+	/// <returns>
+	/// The constant reference to the target.
+	/// </returns>
+	const Target& getTarget() const;
+	
+	/// <summary>
+	/// Returns a reference to the number of bytes written to this sink.
+	/// </summary>
+	/// <returns>
+	/// The reference to the number of bytes.
+	/// </returns>
+	uint32_t& getTotalBytes();
+	
+	/// <summary>
+	/// Returns a constant reference to the number bytes written to this sink.
+	/// </summary>
+	/// <returns>
+	/// The constant reference to the number of bytes.
+	/// </returns>
+	const uint32_t& getTotalBytes() const;
+	
+	/// <summary>
+	/// Returns a reference to the number of tainted bytes written to this 
+	/// sink.
+	/// </summary>
+	/// <returns>
+	/// The reference to the number of tainted bytes.
+	/// </returns>
+	uint32_t& getTotalTaintBytes();
+	
+	/// <summary>
+	/// Returns a constant reference to the number of tainted bytes written to 
+	/// this sink.
+	/// </summary>
+	/// <returns>
+	/// The constant reference to the number of tainted bytes.
+	/// </returns>
+	const uint32_t& getTotalTaintBytes() const;
+	
+	/// <summary>
+	/// Returns a reference to the number of times this sink was written to.
+	/// </summary>
+	/// <returns>
+	/// The reference to the number of times.
+	/// </returns>
+	uint32_t& getTotalWrites();
+	
+	/// <summary>
+	/// Returns a constant reference to the number of times this sink was 
+	/// written to.
+	/// </summary>
+	/// <returns>
+	/// The constant reference to the number of times.
+	/// </returns>
+	const uint32_t& getTotalWrites() const;
+
+	/// <summary>
+	/// Assignment of Target Source instances is forbidden.
+	/// </summary>
+	TargetSink& operator=(const TargetSink&) = delete;
+protected:
+	std::unique_ptr<Target> target;            // Target attached to this sink
+	size_t index;                              // Index of this in sources list
+
+	std::map<uint32_t, uint32_t> labeledBytes; // Map of source target index to
+	                                           // tainted bytes of said source
+	                                           // written to this.
+	
+	uint32_t totalBytes;                       // # of bytes written to
+	uint32_t totalTaintBytes;                  // # of tainted bytes written to
+	uint32_t totalWrites;                      // # of times written to
+};
+
+/// <summary>
+/// Class which represents a source target.
+/// </summary>
+class TargetSource {
+public:
+	/// <summary>
+	/// Constructs a Target Source with the specified target. The index should
+	/// be equivalent to the index of the Target in its sources vector.
+	/// </summary>
+	/// <param name="target">
+	/// The unique pointer to the target.
+	/// </param>
+	/// <param name="index">
+	/// The index of this Target in the sources vector.
+	/// </param>
+	TargetSource(std::unique_ptr<Target> target, const size_t &index);
+
+	/// <summary>
+	/// Copying of Target Source instances is forbidden.
+	/// </summary>
+	TargetSource(const TargetSource&) = delete;
+
+	/// <summary>
+	/// Returns a reference to the index of this target in the sources vector.
+	/// This is expected to never change since targets are not added nor
+	/// removed after the plugin is initialized.
+	/// </summary>
+	const size_t& getIndex() const;
+
+	/// <summary>
+	/// Returns a reference to the number of labeled bytes of this target.
+	/// This should be set when any data of this source target is labeled.
+	/// </summary>
+	/// <returns>
+	/// A reference to the value.
+	/// </returns>
+	uint32_t& getLabeledBytes();
+	
+	/// <summary>
+	/// Returns a constant reference to the number of labeled bytes of this 
+	/// target. This should be set when any data of this source target is 
+	/// labeled.
+	/// </summary>
+	/// <returns>
+	/// A constant reference to the value.
+	/// </returns>
+	const uint32_t& getLabeledBytes() const;
+
+	/// <summary>
+	/// Gets a constant reference to the target attached to this sink.
+	/// </summary>
+	/// <returns>
+	/// The constant reference to the target.
+	/// </returns>
+	const Target& getTarget() const;
+	
+	/// <summary>
+	/// Returns a reference to the number of bytes read from this source.
+	/// </summary>
+	/// <returns>
+	/// The reference to the number of bytes.
+	/// </returns>
+	uint32_t& getTotalBytes();
+	
+	/// <summary>
+	/// Returns a constant reference to the number bytes read from this 
+	/// source.
+	/// </summary>
+	/// <returns>
+	/// The constant reference to the number of bytes.
+	/// </returns>
+	const uint32_t& getTotalBytes() const;
+	
+	/// <summary>
+	/// Returns a reference to the number of times this source was read from.
+	/// </summary>
+	/// <returns>
+	/// The reference to the number of times.
+	/// </returns>
+	uint32_t& getTotalReads();
+	
+	/// <summary>
+	/// Returns a constant reference to the number of times this source was 
+	/// read from.
+	/// </summary>
+	/// <returns>
+	/// The constant reference to the number of times.
+	/// </returns>
+	const uint32_t& getTotalReads() const;
+
+	/// <summary>
+	/// Assignment of Target Source instances is forbidden.
+	/// </summary>
+	TargetSource& operator=(const TargetSource&) = delete;
+protected:
+	std::unique_ptr<Target> target;        // The target attached to this src
+	size_t index;                          // Index of this in sources vector
+
+	uint32_t labeledBytes;                 // # of tainted bytes read from
+	
+	uint32_t totalBytes;                   // # of bytes read from
+	uint32_t totalReads;                   // # of times read from
+};
+
+/// <summary>
+/// Structure which represents a trackable file target.
+/// </summary>
+class TargetFile : public Target {
+public:
+	/// <summary>
+	/// Creates a new, invalid TargetFile.
+	/// </summary>
+	TargetFile();
+
+	/// <summary>
+	/// Creates a new trackable file target with the specified name.
+	/// </summary>
+	/// <param name="name">
+	/// The directory and name of the file target.
+	/// </param>
+	TargetFile(const std::string &name);
+
+	/// <summary>
+	/// Simple method which returns the file name string of this File Target.
+	/// </summary>
+	/// <returns>
+	/// The file name of this File Target.
+	/// </returns>
+	virtual std::string toString() const override;
+	
+	/// <summary>
+	/// Checks if this File Target is valid. A file target is considered valid
+	/// if the file name is non-empty.
+	/// </summary>
+	/// <returns>
+	/// True if the file name of this File Target is non-empty.
+	/// </returns>
+	virtual explicit operator bool() const override;
+	
+	/// <summary>
+	/// Compares the specified Target instance for equality to this.
+	/// </summary>
+	/// <param name="rhs">
+	/// The right hand side of the operator to which this instance is to be
+	/// compared to.
+	/// </param>
+	/// <returns>
+	/// True if the instances are equivalent, false otherwise.
+	/// </returns>
+	virtual bool operator==(const Target &rhs) const override;
+
+	/// <summary>
+	/// Compares the specified Target instance for inequality to this.
+	/// </summary>
+	/// <param name="rhs">
+	/// The right hand side of the operator to which this instance is to be
+	/// compared to.
+	/// </param>
+	/// <returns>
+	/// True if the instances are inequivalent, false otherwise.
+	/// </returns>
+	virtual bool operator!=(const Target &rhs) const override;
+protected:
+	std::string fileName;                      // The File Name of the Target
+};
+
+/// <summary>
+/// Structure which represents a trackable network target.
+/// </summary>
+class TargetNetwork : public Target {
+public:
+	/// <summary>
+	/// Creates a new, invalid TargetNetwork.
+	/// </summary>
+	TargetNetwork();
+
+	/// <summary>
+	/// Creates a new trackable network target with the specified name.
+	/// </summary>
+	/// <param name="ip">
+	/// The IP address of the network target.
+	/// </param>
+	/// <param name="port">
+	/// The port of the network target.
+	/// </param>
+	TargetNetwork(const std::string &ip, const unsigned int &port);
+
+	/// <summary>
+	/// Simple method which returns a string representation of this Network 
+	/// Target.
+	/// </summary>
+	/// <returns>
+	/// The string in format: "$ip$::$port$".
+	/// </returns>
+	virtual std::string toString() const override;
+	
+	/// <summary>
+	/// Checks if this Network Target is valid. A network target is considered
+	/// valid if the IP address string is non-empty.
+	/// </summary>
+	/// <returns>
+	/// True if the IP address of this Network Target is non-empty.
+	/// </returns>
+	virtual explicit operator bool() const override;
+
+	/// <summary>
+	/// Compares the specified Target instance for equality to this.
+	/// </summary>
+	/// <param name="rhs">
+	/// The right hand side of the operator to which this instance is to be
+	/// compared to.
+	/// </param>
+	/// <returns>
+	/// True if the instances are equivalent, false otherwise.
+	/// </returns>
+	virtual bool operator==(const Target &rhs) const override;
+
+	/// <summary>
+	/// Compares the specified Target instance for inequality to this.
+	/// </summary>
+	/// <param name="rhs">
+	/// The right hand side of the operator to which this instance is to be
+	/// compared to.
+	/// </param>
+	/// <returns>
+	/// True if the instances are inequivalent, false otherwise.
+	/// </returns>
+	virtual bool operator!=(const Target &rhs) const override;
+protected:
+	std::string ip;                            // The IP Address of the Target
+	unsigned short port;                       // The Port of the Target
+};
 
 typedef std::pair<target_ulong, uint32_t> FD_ASID_Pair;
 
